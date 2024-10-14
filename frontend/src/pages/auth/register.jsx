273a762f19277052df7a6cada1f8a637717306ba
BarakaCaleb <calebbaraka79@gmail.com>
2024-10-14 20:37:18 +0300

    chore: 404 Error page gif updates

--- frontend/src/pages/auth/register.jsx
+++ frontend/src/pages/auth/register.jsx
@@ -38,9 +38,9 @@ function AuthRegister() {
   console.log(formData);
 
   return (
-    <div className="mx-auto w-full max-w-md space-y-6">
+    <div className="mx-auto w-full max-w-md space-y-6 bg-green-200">
       <div className="text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-foreground text-colo">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
           Create new account
         </h1>
         <p className="mt-2">
--- frontend/src/pages/not-found/index.jsx
+++ frontend/src/pages/not-found/index.jsx
@@ -1,5 +1,15 @@
 function NotFound() {
-  return <div>page doesn't exists</div>;
+  return (
+    <div className="not-found-container">
+      <h1>Oops! Page Not Found</h1>
+      <img 
+        src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif" 
+        alt="Not Found" 
+        className="not-found-gif" 
+      />
+      <p>It seems like you've lost your way. Let's get you back home.</p>
+    </div>
+  );
 }
 
 export default NotFound;
