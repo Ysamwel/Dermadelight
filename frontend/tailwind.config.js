dd11ed1472dfb9e30dcc28265d832012e8a318b3
BarakaCaleb <calebbaraka79@gmail.com>
2024-10-14 20:09:37 +0300

    custom theme config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        mintGreen: "#98FF98",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
